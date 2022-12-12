2975f14987353793cd0bb1854c60e30d57b15700
Marco <marco.rubio1994@gmail.com>
2022-12-12 13:47:18 -0600

    Se crea el código del primer ejercicio con strings

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//CharAt accede a los subindices
+let nombre = "Marco Antonio Rubio Gonzalez";
+nombre = nombre.toUpperCase();
+let letra="";
+let contFinal =0;
+//Recorro la cadena 
+for (let index = 0; index < nombre.length; index++) {
+    console.log(nombre.charAt(index)); 
+let contTemp = 1;
+    for(let cont =index+1; cont < nombre.length; cont++)  {
+    if(nombre.charAt(index) ==nombre.charAt(cont))
+      
+        contTemp++;
+    }
+    if(contTemp>contFinal){
+        contFinal=contTemp;
+        letra = nombre.charAt(index);
+    }
+}
+console.log(letra, contFinal);
